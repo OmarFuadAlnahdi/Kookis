73e7815264355cdab92c288309aa274e426b668e
OmarFuadAlnahdi <devumar0@hotmail.com>
2024-06-12 00:17:23 +0300

    Fixing bug

--- ViewModels/PiesListViewModel.cs
+++ ViewModels/PiesListViewModel.cs
@@ -7,11 +7,9 @@ namespace Kookis.ViewModels
         public string? CurrentCategory { get;}
         public string ForWho { get; }
 
-        public PiesListViewModel (IEnumerable<Pie> pies, string? currentCategory,string forWho)
+        public PiesListViewModel (IEnumerable<Pie> pies)
         {
             Pies = pies;
-            CurrentCategory = currentCategory;
-            ForWho = forWho;
         }
         
     }
