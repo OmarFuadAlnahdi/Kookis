763e5ff63b025f13cd85d05c7d5603f98fe6ef95
OmarFuadAlnahdi <devumar0@hotmail.com>
2024-06-24 11:36:46 +0300

    adding a home page

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Kookis.Models;
+using Kookis.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kookis.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository) 
+        {
+            _pieRepository = pieRepository;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            var piesOFTheWeek = _pieRepository.PiesOfTheWeek;
+            var homeViewModel = new HomeViewModel(piesOFTheWeek);
+            return View(homeViewModel);
+        
+        }
+    }
+}
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Kookis.Models;
+
+namespace Kookis.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable <Pie> PiesOfTheWeek { get; set;}
+
+        public HomeViewModel(IEnumerable<Pie> piesOFTheWeek)
+        {
+            PiesOfTheWeek = piesOFTheWeek;
+        }
+    }
+}
