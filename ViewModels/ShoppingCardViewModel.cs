ff5092877dd98c091e074993495ae38981e53c51
OmarFuadAlnahdi <devumar0@hotmail.com>
2024-06-26 08:59:09 +0300

    Adding viewModel For the shopping card

---
+++ ViewModels/ShoppingCardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Kookis.Models;
+
+namespace Kookis.ViewModels
+{
+    public class ShoppingCardViewModel
+    {
+        public ShoppingCardViewModel(IShoppingCart shoppingCart, decimal shoppingCartTotal) 
+        {
+            ShoppingCart = shoppingCart;
+            ShoppingCartTotal = shoppingCartTotal;
+        }
+        public IShoppingCart ShoppingCart { get; }
+        public decimal ShoppingCartTotal { get; }
+    }
+}
