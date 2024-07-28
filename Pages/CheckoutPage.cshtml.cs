8d0abeb9b68aa961855a28d6f54ea358feeaf3c0
OmarFuadAlnahdi <devumar0@hotmail.com>
2024-07-28 11:44:38 +0300

    using pages in the app

---
+++ Pages/CheckoutPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Kookis.Pages
+{
+    public class CheckoutPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddSession();
 builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 builder.Services.AddDbContext<KookisDbContext>(options =>
 {
     options.UseSqlServer(
@@ -30,5 +31,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapDefaultControllerRoute();
+
+app.MapRazorPages();
+
 DbInitializer.Seed(app);
 app.Run();
