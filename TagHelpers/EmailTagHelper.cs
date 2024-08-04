5f8a5324d2874b4c9b8ea7b77403f279eef7c797
OmarFuadAlnahdi <devumar0@hotmail.com>
2024-08-05 00:09:51 +0300

    setting up the api

--- TagHelpers/EmailTagHelper.cs
+++ TagHelpers/EmailTagHelper.cs
@@ -5,13 +5,13 @@ namespace Kookis.TagHelpers
     public class EmailTagHelper : TagHelper //this class will be as element like this <email/>
     {
         public string? Address { get; set; }
-        public string? Contect { get; set; }
+        public string? Content { get; set; }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "a";
             output.Attributes.SetAttribute("href", "mailto" + Address);
-            output.Content.SetContent(Contect);
+            output.Content.SetContent(Content);
         }
     }
 }
