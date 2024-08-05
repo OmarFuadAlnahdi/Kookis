104c7c232cb20f9efd7d872e50cc692e577b95c5
OmarFuadAlnahdi <devumar0@hotmail.com>
2024-08-05 18:48:01 +0300

    Changing some of the code

--- Controllers/Api/SearchController.cs
+++ Controllers/Api/SearchController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Kookis.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Kookis.Controllers.Api
@@ -7,5 +8,23 @@ namespace Kookis.Controllers.Api
     [ApiController]
     public class SearchController : ControllerBase
     {
+        public IPieRepository _pieRepository {  get; set; }
+        public SearchController(IPieRepository pieRepository) 
+        {
+            _pieRepository = pieRepository;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAll() 
+        {
+            
+            
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id) 
+        {
+            
+        }
     }
 }
